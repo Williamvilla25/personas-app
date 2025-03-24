30a3e19813b94a4aff69903cb950893cf08d3bfa
Williamvilla25 <williamvilla727@gmail.com>
2025-03-24 15:58:35 -0500

    Creacion de Vista para comunas

--- app/Models/Comuna.php
+++ app/Models/Comuna.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Comuna extends Model
 {
     use HasFactory;
+    protected $table = 'tb_comuna';
+    protected $primarykey = 'comuna_codi';
+    public $timetamps = false;
 }
+
