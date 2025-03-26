f05e7d5c0c98496a6d05e68d97cfc81990ac508d
Williamvilla25 <williamvilla727@gmail.com>
2025-03-26 14:04:03 -0500

    creacion del index y rutas para departamento

--- app/Http/Controllers/DepartamentoController.php
+++ app/Http/Controllers/DepartamentoController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Departamento;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class DepartamentoController extends Controller
 {
@@ -13,7 +15,8 @@ class DepartamentoController extends Controller
      */
     public function index()
     {
-        //
+        $departamentos = Departamento::all();
+        return view('departamento.index', ['departamento' =>$departamentos]);
     }
 
     /**
