7c8bafaf034752a66577e197e86990b55d91a034
Williamvilla25 <williamvilla727@gmail.com>
2025-03-25 20:10:02 -0500

    Ajuste de la vista del listado de municipios

--- app/Models/Departamento.php
+++ app/Models/Departamento.php
@@ -5,10 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Municipio extends Model
+class Departamento extends Model
 {
     use HasFactory;
-    protected $table = "tb_municipio";
-    protected $primarykey = 'muni_codi';
-    public $timmetamps = false;
 }
