d1cd48aa09fed3c8eac70be7e5fd520129eba433
Williamvilla25 <williamvilla727@gmail.com>
2025-05-21 20:30:18 -0500

    Creacion api comunas

---
+++ app/Http/Controllers/api/ComunaController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Models\Comuna;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ComunaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $comunas = DB::table('tb_comuna')
+            ->join('tb_municipio', 'tb_comuna.muni_codi', '=', 'tb_municipio.muni_codi')
+            ->select('tb_comuna.*', 'tb_municipio.muni_nomb')
+            ->get();
+
+        return $request->expectsJson()
+            ? response()->json($comunas)
+            : view('comuna.index', ['comunas' => $comunas]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $comuna = new Comuna();
+        $comuna->comu_nomb = $request->comu_nomb;
+        $comuna->muni_codi = $request->muni_codi;
+        $comuna->save();
+        return json_encode(['comuna' => $comuna]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $comuna = Comuna::find($id);
+        $comuna->comu_nomb = $request->comu_nomb;
+        $comuna->muni_codi = $request->muni_codi;
+        $comuna->save();
+
+        return json_encode(['comuna' => $comuna]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $comuna = Comuna::find($id);
+        $comuna->delete();
+        $comuna = DB::table('tb_comuna')
+            ->join('tb_municipio', 'tb_comuna.muni_codi', '=', 'tb_municipio.muni_codi')
+            ->select('tb_comuna.*', 'tb_municipio.muni_nomb')
+            ->get();
+
+        return json_encode(['comunas' => $comuna, 'success' => true]);
+    }
+}
